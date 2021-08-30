e21c799dc9043547caa5cfde2830083db4a398b5
Calum Davidson <65898287+chdavidson@users.noreply.github.com>
2021-08-30 16:08:19 +0100

    change file heirarchy -> server side straight into route folder instead of inside server folder

--- Client/src/Components/GiftAidOptIn.js
+++ Client/src/Components/GiftAidOptIn.js
@@ -3,11 +3,12 @@ import React from 'react'
 import { CONSTANTS } from '../CONSTANTS';
 
 
-const GiftAidOptIn = () => {
+const GiftAidOptIn = ({handleOptIn, donation}) => {
 
 
     return (
         <div className='gift-aid-container'>
+            {donation}
             <p className='gift-aid-decalration'>{CONSTANTS.GIFT_AID_DECLARATION}</p>
         </div>
     )
